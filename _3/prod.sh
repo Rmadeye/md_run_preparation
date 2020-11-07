7954d27a7ff5ca2c8339ac08ca1686a909f1b987
rmadeye <rafmadaj@gmail.com>
2020-11-07 18:35:22 +0100

    Some path and name fixes

--- _3/prod.sh
+++ _3/prod.sh
@@ -7,8 +7,8 @@ for i in {1..10}; do
 echo 'Job number being done:' $i
 output="$filename"\_$i.nc
 
-pmemd.cuda -O -i ../MD_cfg/prod.in -o prod.out -p ../parms/lig-prot-solv.parm7 -c ../rst7s/lig-prot-solv_heat.rst7 -r lig-prot-solv_"$i".rst7 -inf info.inf -x "$output"
+pmemd.cuda -O -i ../MD_cfg/prod.in -o prod.out -p ../parms/lig-prot-solv.parm7 -c ../rst7s/lig-prot-solv_heat.rst7 -r ../rst7s/lig-prot-solv_"$i".rst7 -inf info.inf -x "$output"
 
 done
 
-cpptraj -i ../MD_cfg/cpptraj_prepare_and_analyze.in
+cpptraj -i ../MD_cfg/analyze_cpptraj.in
--- prepare_md_dir.sh
+++ prepare_md_dir.sh
@@ -6,8 +6,9 @@ mkdir $dirname
 
 cd $dirname
 
-cp -r ~/bash/MD_prep/_1 ~/bash/MD_prep/_2 ~/bash/MD_prep/_3 ~/bash/MD_prep/MMGBSA/ ./
+cp -r ~/bash/MD_prep/_1 ~/bash/MD_prep/_2 ~/bash/MD_prep/_3 ~/bash/MD_prep/MMGBSA/ ~/bash/MD_prep/parms/ ~/bash/MD_prep/rst7s/ ~/bash/MD_prep/postprocessing/ ./
 cp -r ~/bash/MD_prep/MD_cfg/ ./
+cp -r ~/bash/MD_prep/python_scripts/ ./
 
 
 
