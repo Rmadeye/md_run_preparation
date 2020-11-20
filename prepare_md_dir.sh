d927cceb3e9745f84ef428fa3fe86073a32a3010
rmadeye <rafmadaj@gmail.com>
2020-11-20 08:38:51 +0100

    Dir update

--- prepare_md_dir.sh
+++ prepare_md_dir.sh
@@ -5,6 +5,7 @@ read -p 'Directory name: ' dirname
 mkdir $dirname
 
 cd $dirname
+mkdir parms postprocessing rst7s
 
 cp -r ~/bash/MD_prep/_1 ~/bash/MD_prep/_2 ~/bash/MD_prep/_3 ~/bash/MD_prep/MMGBSA/ ~/bash/MD_prep/parms/ ~/bash/MD_prep/rst7s/ ~/bash/MD_prep/postprocessing/ ./
 cp -r ~/bash/MD_prep/MD_cfg/ ./
