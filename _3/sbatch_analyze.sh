5039d01f7a4ae57a1889bd66b52afd39882ae9e0
rmadeye <rafmadaj@gmail.com>
2021-01-14 12:08:55 +0100

    Prometheus cluster adjusted

---
+++ _3/sbatch_analyze.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH -J basic_analysis
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=rmadaj@cbmm.lodz.pl
+#SBATCH --time=1:00:00
+#SBATCH -A plgmadeyemd
+#SBATCH --partition=plgrid-short
+
+module load plgrid/apps/amber/18
+
+cpptraj -i ../MD_cfg/check_2.in
