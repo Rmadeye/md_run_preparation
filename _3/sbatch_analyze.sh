4a20f43904b1b8e0b2f8a00dbe5c349648c61018
rmadeye <rafmadaj@gmail.com>
2021-01-17 15:16:22 +0100

    Prometheus cluster adjusted Basic validation applied

--- _3/sbatch_analyze.sh
+++ _3/sbatch_analyze.sh
@@ -2,8 +2,6 @@
 #SBATCH -J basic_analysis
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=rmadaj@cbmm.lodz.pl
 #SBATCH --time=1:00:00
 #SBATCH -A plgmadeyemd
 #SBATCH --partition=plgrid-short
@@ -11,3 +9,7 @@
 module load plgrid/apps/amber/18
 
 cpptraj -i ../MD_cfg/analyze_cpptraj.in
+
+module load plgrid/tools/python/3.8
+
+python3 ../python_scripts/basic_validation.py -i *.csv
