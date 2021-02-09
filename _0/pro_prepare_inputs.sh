88e274c4ceb37ca9182f7697d0a7a96483603ea0
rmadeye <rafmadaj@gmail.com>
2021-02-09 21:27:00 +0100

    Huge update: _0 as preprocessing of files custom grants clustering added

--- _0/pro_prepare_inputs.sh
+++ _0/pro_prepare_inputs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 module load plgrid/apps/amber/18 || source ~/amber20_src/amber.sh
-ambpdb -p ../parm7/lig-prot-solv.parm7 -c ../rst7s/lig-prot-solv.rst7 > input_complex.pdb
+ambpdb -p ../parms/lig-prot-solv.parm7 -c ../rst7s/lig-prot-solv.rst7 > input_complex.pdb
 read -p 'Set your grant name for CPU jobs: ' grantname
 find .. -name "*.sh" -exec sed -i "s/grantid/$grantname/g" {} \;
 echo $grantname set as default CPU grant
