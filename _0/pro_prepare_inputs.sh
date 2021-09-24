a9297240580b38c6e05783e348f3e87b53133884
rmadeye <rafmadaj@gmail.com>
2021-09-24 14:23:22 +0200

    started_main_shell_file

--- _0/pro_prepare_inputs.sh
+++ _0/pro_prepare_inputs.sh
@@ -10,32 +10,49 @@ echo $grantnamegpu set as default GPU grant
 read -p 'Set number of cores for plgrid-short : ' ncpus_short
 find .. -name "*.sh" -exec sed -i "s/nodesnumber_short/$ncpus_short/g" {} \;
 echo $ncpus_short set as number of cores for plgrid-short partition
+
+read -p 'Is ligand present? y/n: ' ligand_presence
+if [ $ligand_presence = 'y' ]; then
 read -p 'Set name of the ligand unless standard (UNL, MOL, UNK) : ' ligname
-find .. -name "*.py" -exec sed -i "s/DEF/$ligname/g" {} \;
-echo $ligname set as ligand name
+#find .. -name "*.py" -exec sed -i "s/DEF/$ligname/g" {} \;
+ligand_index="$(grep -i $ligname input_complex.pdb | awk '//{print $5}' | uniq | head -n 1)"
+atom_count="$(grep -i $ligname input_complex.pdb | awk '//{print $2}' | tail -n 2 | head -n 1)"
+echo $ligname set as ligand name, $atom_count set as printed number of atoms
+echo "Preparing input files..."
+read -p "Set filename for results: " results_filename
+echo Preparing residue indexes in MM/GB(PB)SA and CPPTRAJ input files...
+find ./MMGBSA/ -name "*.sh" -exec sed -i "s/complex/$ligand_index/g" {} \;
 
 
-python3 ../python_scripts/prepare_inputs.py
-ante-MMPBSA.py -p ../parms/lig-prot-solv.parm7 -c ../parms/stripped.lig-prot-solv.parm7 -s ':WAT,:Na+,:Cl-'
-#read -p 'Set production time to be analyzed (ns) : ' prodtime
-#find .. -name "*.*" -exec sed -i "s/productiontime/$prodtime/g" {} \;
-#echo $prodtime set as time for basic analysis
-read -p 'Set number of residues for RMSF analysis : ' rmsfresidues
-find .. -name "*.py" -exec sed -i "s/rmsfresidues/$rmsfresidues/g" {} \;
-echo $rmsfresidues set as number of residues for RMSF study
-read -p 'Set residue/ligand name for cluster analysis : ' clusname
-find .. -name "*.*" -exec sed -i "s/clusname/$clusname/g" {} \;
-echo $clusname set as cluster analysis reference
-echo "***Set MM/GB(PB)SA calculations***"
-read -p 'Set igb for MMGB(PB)SA:  (8 for anything but phosphates)' igb
-find .. -name "*.in" -exec sed -i "s/igbset/$igb/g" {} \;
-read -p 'Set calculation frame interval for MMGB(PB)SA:  (Default 10)' interval
-find .. -name "*.in" -exec sed -i "s/intervalset/$interval/g" {} \;
-read -p 'Set number of cpus used for MMGB(PB)SA calculations:  (Default 10)' ncpu_long
-find .. -name "*.sh" -exec sed -i "s/mmgbsacpu/$ncpu_long/g" {} \;
-echo "MMGB(PB)SA features set: ncpus $ncpu_long, igb = $igb, interval = $interval"
-find .. -name "*.sh" -exec sed -i "s/cluster-name/`basename ${PWD%/*}`/g" {} \;
-find .. -name "mmgbsa_local.sh" -exec sed -i "s/inputname/`basename ${PWD%/*}`/g" {} \;
+
+elif [ $ligand_presence = 'n' ]; then
+echo "dupa"
+else
+echo "incorrect answer, quitting..."
+fi
+
+
+# python3 ../python_scripts/prepare_inputs.py
+# ante-MMPBSA.py -p ../parms/lig-prot-solv.parm7 -c ../parms/stripped.lig-prot-solv.parm7 -s ':WAT,:Na+,:Cl-'
+# #read -p 'Set production time to be analyzed (ns) : ' prodtime
+# #find .. -name "*.*" -exec sed -i "s/productiontime/$prodtime/g" {} \;
+# #echo $prodtime set as time for basic analysis
+# read -p 'Set number of residues for RMSF analysis : ' rmsfresidues
+# find .. -name "*.py" -exec sed -i "s/rmsfresidues/$rmsfresidues/g" {} \;
+# echo $rmsfresidues set as number of residues for RMSF study
+# read -p 'Set residue/ligand name for cluster analysis : ' clusname
+# find .. -name "*.*" -exec sed -i "s/clusname/$clusname/g" {} \;
+# echo $clusname set as cluster analysis reference
+# echo "***Set MM/GB(PB)SA calculations***"
+# read -p 'Set igb for MMGB(PB)SA:  (8 for anything but phosphates)' igb
+# find .. -name "*.in" -exec sed -i "s/igbset/$igb/g" {} \;
+# read -p 'Set calculation frame interval for MMGB(PB)SA:  (Default 10)' interval
+# find .. -name "*.in" -exec sed -i "s/intervalset/$interval/g" {} \;
+# read -p 'Set number of cpus used for MMGB(PB)SA calculations:  (Default 10)' ncpu_long
+# find .. -name "*.sh" -exec sed -i "s/mmgbsacpu/$ncpu_long/g" {} \;
+# echo "MMGB(PB)SA features set: ncpus $ncpu_long, igb = $igb, interval = $interval"
+# find .. -name "*.sh" -exec sed -i "s/cluster-name/`basename ${PWD%/*}`/g" {} \;
+# find .. -name "mmgbsa_local.sh" -exec sed -i "s/inputname/`basename ${PWD%/*}`/g" {} \;
 
 
 
