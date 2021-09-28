b511bdcfd0867823d1241ee5fae1a0edc6fa2062
rmadeye <rafmadaj@gmail.com>
2021-09-28 15:44:53 +0200

    fully converted to bash

--- postprocessing/cluster_local.sh
+++ postprocessing/cluster_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-cp ../parms/stripped.lig-prot-solv.parm7 merged_centered.nc ../MD_cfg/cluster_cpptraj.in ./
+cp ../parms/stripped.lig-prot-solv.parm7 merged_centered.nc ../MD_cfg/cpptraj_cluster.in ./
 
-mpirun -np 8 cpptraj.MPI -i cluster_cpptraj.in
+mpirun -np 8 cpptraj.MPI -i cpptraj_cluster.in
 
 
