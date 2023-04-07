d61130767a3e408596872e78af6fae8fe77d38be
rmadeye <rafmadaj@gmail.com>
2023-04-07 13:16:28 +0200

    upd

--- _1/min_sbatch.sh
+++ _1/min_sbatch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p cpu          # CPU partition
-#SBATCH -n 1            # 8 cores
+#SBATCH -n 8            # 8 cores
 #SBATCH --exclude=edi00
 #SBATCH --mem=8GB      # 8 GB of RAM
 #SBATCH -J minimisation_rmadeye     # name of your job
@@ -9,7 +9,7 @@
 source /opt/apps/amber18/amber.sh
 
 echo "system minimalisation"
-mpirun -np 8  pmemd.MPI -O -i ../MD_cfg/min_system.in -o min3.out -p ../parms/topology.parm7 -c ../rst7s/coordinates_prot.rst7 -r ../rst7s/coordinates_min.rst7 -inf info.inf 
+mpirun -np 8  pmemd.MPI -O -i ../MD_cfg/min_system.in -o min3.out -p ../parms/topology.parm7 -c ../rst7s/coordinates.rst7 -r ../rst7s/coordinates_min.rst7 -inf info.inf 
 
 ambpdb -p ../parms/topology.parm7 -c ../rst7s/coordinates_min.rst7 > integrity_check.pdb
 
